7cca0ee2f580fb1892b141b4bff73a8ec09750d3
tectoshi <116927616+tectoshi@users.noreply.github.com>
2022-12-15 17:34:52 +0900

    横棒グラフの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
   def show
-    @data = [['2019-06-01', 100], ['2019-06-02', 200], ['2019-06-03', 150]]
+    nutrient = Nutrient.find(1)
+    
+    @calorie = nutrient.calorie
+    @protein = nutrient.protein
+    @lipid = nutrient.lipid
+    @carbohydrate = nutrient.carbohydrate
+    @sugar = nutrient.sugar
+    @fiber = nutrient.fiber
+    @nutrient_sum = {name: @name, calorie: @calorie, protein: @protein, lipid: @lipid, carbohydrate: @carbohydrate, sugar: @sugar, fiber: @fiber, number: @num, user_id: @user_id}
   end
 end
