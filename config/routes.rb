91f097baf63f8f3d1f91f5140d36cd121f90f085
tectoshi <116927616+tectoshi@users.noreply.github.com>
2022-12-22 15:09:06 +0900

    deviseのusersコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+  }
+  
   root "nutrients#index"
   resources :nutrients
   resources :users
