284e9ac96c83f19d1b1e4ca250cda9cbb2e8af8e
tectoshi <116927616+tectoshi@users.noreply.github.com>
2022-12-27 02:59:32 +0900

    手動デプロイ設定

--- app/models/second.rb
+++
@@ -1,7 +0,0 @@
-class Second < ActiveHash::Base
-  self.data = [
-    { id: 1, name: '---' },
-    { id: 2, name: '', calorie:, protein:, lipid:, carbohydrate:, sugar:, fiber:},
-    { id: 3, name: '', calorie:, protein:, lipid:, carbohydrate:, sugar:, fiber:}
-  ]
-end
